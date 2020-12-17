bdfe57bb6a55946f44b97ac4dc96e289f3cde94e
surajadkhari <surajadkhari@gmail.com>
2020-12-17 14:37:03 +0545

    Missing of Day2

--- lib/screens/widgetrefactor.dart
+++ lib/screens/widgetrefactor.dart
@@ -6,17 +6,47 @@ class Multiplewidget extends StatefulWidget {
 }
 
 class _MultiplewidgetState extends State<Multiplewidget> {
+  bool checkboxValue = false;
+  bool radioValue = true;
+  double sliderValue = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBarBuilder(),
       body: Column(
         children: [
-          for (int i = 0; i < 9; i++) _eachButtonBuilder(i.toString()),
+          Column(
+            children: [
+              for (int i = 0; i < 9; i++) _eachButtonBuilder(i.toString()),
 
-          // _eachButtonBuilder('0'),
-          // _eachButtonBuilder('1'),
-          // _eachButtonBuilder('2'),
+              // _eachButtonBuilder('0'),
+              // _eachButtonBuilder('1'),
+              // _eachButtonBuilder('2'),
+            ],
+          ),
+          Checkbox(
+            value: checkboxValue,
+            onChanged: (value) {
+              checkboxValue = value;
+              setState(() {});
+            },
+          ),
+          Radio(
+            value: radioValue,
+            groupValue: true,
+            onChanged: (value) {},
+          ),
+          Slider(
+            divisions: 5,
+            activeColor: Colors.red,
+            max: 10,
+            value: sliderValue,
+            onChanged: (value) {
+              setState(() {
+                sliderValue = value;
+              });
+            },
+          )
         ],
       ),
     );
